c7add62ed32d432ffdc2be0293fe29680f45b744
OwenStadlwieser <47876306+OwenStadlwieser@users.noreply.github.com>
2023-03-21 14:48:28 -0600

    .

---
+++ docker-images/autoscaler/myapp.py
@@ -0,0 +1,42 @@
+import random
+import time
+import pandas
+import statistics
+import docker
+from flask import Flask
+import requests
+app = Flask(__name__)
+
+IMAGE_NAME = "web_app"
+LOWER_BOUND = 20
+HIGHER_BOUND = 50
+def calc_response_time():
+    pass
+
+# def create_service():
+#     docker.service.create(IMAGE_NAME, )
+
+# def set_number_of_replicas(num_reps: int):
+    
+#     docker.service.update(mode={'Replicated': {'Replicas': new_replicas}}).create()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
+
+@app.route('/')
+def hello():
+      start_time = time.time()
+      r = requests.post('http://web:8000')
+      end_time = time.time()
+      res_time = start_time - end_time
+      if (res_time < LOWER_BOUND):
+        pass
+      elif (res_time > HIGHER_BOUND):
+        pass   
+      else: 
+        pass
+      print("Respone time: ", res_time)
+      return r
+
+           
