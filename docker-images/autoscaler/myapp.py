42c009674304487ffb14afac35e069a38a2bb1c9
OwenStadlwieser <47876306+OwenStadlwieser@users.noreply.github.com>
2023-04-14 12:50:07 -0600

    Update myapp.py

--- docker-images/autoscaler/myapp.py
+++ docker-images/autoscaler/myapp.py
@@ -2,37 +2,52 @@ import docker
 from flask import Flask
 import requests
 import time
+from threading import Thread
+from time import sleep
+
+
 app = Flask(__name__)
 
 SERVICE_NAME = "app_name_web"
 
+epsilon = 5
 
-import time
+print("In app")
 
 class DockerController:
     def __init__(self, service_name):
         self.service_name = service_name
         self.client = docker.DockerClient(base_url='unix:///var/run/docker.sock')
-        self.service = self.client.services.get(self.service_name)
+        # update_config = docker.types.UpdateConfig(
+        #     parallelism=2,
+        #     delay=10
+        # )
+        # print("Here")
+        # try:
+        #   self.service.update(update_config=update_config)
+        # except Exception as e:
+        #   print(e)
         print("Getting replicas")
         self.number_of_replicas = self.get_number_of_replicas()
         print("Got replicas")
 
-    def decrement_num_reps(self):
+    def decrement_num_reps(self, reset = False):
         if self.number_of_replicas > 1:
+          num_reps = 1 if reset else self.number_of_replicas - 1
           print("Decrementing number of replicas")
-          self.service.reload()
-          self.service.update(mode={'Replicated': {'Replicas': self.number_of_replicas - 1}})
-          self.number_of_replicas = self.number_of_replicas - 1
+          service = self.client.services.get(self.service_name)
+          service.scale(num_reps)
+          self.number_of_replicas = num_reps
 
     def increment_num_reps(self):
         print("Incrementing number of replicas")
-        self.service.reload()
-        self.service.update(mode={'Replicated': {'Replicas': self.number_of_replicas + 1}})
+        service = self.client.services.get(self.service_name)
         self.number_of_replicas = self.number_of_replicas + 1
+        service.scale(self.number_of_replicas)
 
     def get_number_of_replicas(self):
-        return self.service.attrs['Spec']['Mode']['Replicated']['Replicas']
+        service = self.client.services.get(self.service_name)
+        return service.attrs['Spec']['Mode']['Replicated']['Replicas']
 
 class AverageCalculator:
     def __init__(self, sample_time=15):
@@ -41,31 +56,47 @@ class AverageCalculator:
         self.last_sample_start = time.time()
         self.docker_controller = DockerController(SERVICE_NAME)
         self.response_times_per_replica = []
-        self.baseline_average = 1.5
+        self.baseline_average = 1.6
         self.prev_average = 0
         self.num_replicas = self.docker_controller.number_of_replicas
 
     def is_window_expired(self):
-      if time.time() - self.last_sample_start > self.sample_time:
+      if (time.time() - self.last_sample_start) > self.sample_time:
         return True
     
     def get_average(self):
-      if self.is_window_expired():
-        self.last_sample_start = time.time()
+      self.last_sample_start = time.time()
+      print("Times")
+      print(self.times)
+      print(self.response_times_per_replica)
+      if self.times and len(self.times) > 0:
         average = sum(self.times) / len(self.times)
         self.times = []
-        if (average < self.baseline_average):
+        self.response_times_per_replica = []
+        if (average < self.baseline_average or average < ( self.prev_average - epsilon )):
           self.docker_controller.decrement_num_reps()
-        elif (average > self.baseline_average):
+        elif (average > self.baseline_average or average > ( self.prev_average + epsilon )):
           self.docker_controller.increment_num_reps()
         self.prev_average = average
+      else:
+        self.docker_controller.decrement_num_reps(True)
+
 
     def update(self, process_variable):
+      print("Update")
       self.times.append(process_variable)
       self.response_times_per_replica.append(process_variable/self.num_replicas)
-      self.get_average()
-        
+
+
 averageCalculator = AverageCalculator()
+  
+def check_loop():
+  while True:
+    print("Getting average")
+    averageCalculator.get_average()
+    sleep(5)
+    
+
 
 @app.route('/')
 def hello():
@@ -78,6 +109,8 @@ def hello():
   return r.text
 
 if __name__ == "__main__":
+  Thread(target=check_loop, daemon=True).start()
+
   app.run(host="0.0.0.0", port=8000, debug=True)
 
 
